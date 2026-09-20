06d5ac33ba4d166d2670036087b7a4a395a41792
Palmmy2351 <paramimakmaroeng@gmail.com>
2026-09-20 16:39:21 +0700

    Add Java project

---
+++ MyFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class MyFrame extends JFrame {
+    MyFrame() {
+        setTitle("My Frame");
+        setSize(700, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
+    
+
