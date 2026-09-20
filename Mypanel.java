d3bedb498aa37db3e80c615277aa217f3822a1a3
Palmmy2351 <paramimakmaroeng@gmail.com>
2026-09-20 16:56:04 +0700

    Add Mypanel.java

---
+++ Mypanel.java
@@ -0,0 +1,11 @@
+public class Mypanel extends Thread {
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
