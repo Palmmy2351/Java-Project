b440842b6b47e0cddabedb396903881749cda387
Palmmy2351 <paramimakmaroeng@gmail.com>
2026-09-21 22:13:08 +0700

    Summary

--- MyFrame.java
+++ MyFrame.java
@@ -4,6 +4,13 @@ public class MyFrame extends JFrame {
         setTitle("My Frame");
         setSize(700, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon icon = new ImageIcon("background.jpg");
+        JLabel background = new JLabel(icon);
+        
+        background.setBounds(0, 0, 700, 800);
+        add(background);
+        
         setVisible(true);
     }
 }
