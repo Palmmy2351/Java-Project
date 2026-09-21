67e11bffdd3c9d8344f9a444fe9f832d8e6e0908
Palmmy2351 <paramimakmaroeng@gmail.com>
2026-09-21 22:41:47 +0700

    Summary

--- MyFrame.java
+++ MyFrame.java
@@ -1,18 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
+import java.io.*;
 public class MyFrame extends JFrame {
     MyFrame() {
-        setTitle("My Frame");
-        setSize(700, 800);
+        setSize(690, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        ImageIcon icon = new ImageIcon("background.jpg");
-        JLabel background = new JLabel(icon);
-        
-        background.setBounds(0, 0, 700, 800);
-        add(background);
-        
-        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+     public static void main(String[] args) {
+        MyFrame frame = new MyFrame();
+        frame.add(new MyPanel());
+        frame.setVisible(true);
     }
 }
-    
+ class MyPanel extends JPanel {
+    Image bg = Toolkit.getDefaultToolkit().createImage(
+        System.getProperty("user.dir") + File.separator + "background.jpg" 
+    );
+    MyPanel() {
+        setSize(690, 800);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(bg, 0, 0, this);
+    }     
+ }
 
