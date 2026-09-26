033f769a144490a2174621ddfcdecf5fbeb6160a
NikaGitGit <pattapeey11@gmail.com>
2026-09-26 15:07:51 +0700

    Summary

--- MyPanel.java
+++ MyPanel.java
@@ -7,7 +7,25 @@ public class Mypanel extends JPanel{
     Image bg = Toolkit.getDefaultToolkit().createImage(
         System.getProperty("user.dir") + File.separator + "background.jpg" 
     );
-    Image meteor = randomImage();
+    Image meteor [] ;
+    int meteorX [];
+    int meteorY [];
+    int meteorCount;
+
+    public void setMeteorCount(int count){
+        meteorCount = count;
+        meteor = new Image[count];
+        meteorX = new int[count];
+        meteorY = new int[count];
+        for (int i = 0; i < count; i++) {
+            meteor[i] = randomImage();
+            meteorX[i] = (int)(Math.random()*640);
+            meteorY[i] = (int)(Math.random()*750);
+
+        }
+        repaint();
+    }
+
     Mypanel() {
         setSize(690, 800);
     }
@@ -15,7 +33,10 @@ public class Mypanel extends JPanel{
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(bg, 0, 0, this);
-        g.drawImage(meteor, 100, 100,50, 50, this);
+        for (int i = 0; i < meteorCount; i++) {
+            g.drawImage(meteor[i],meteorX[i],meteorY[i],50, 50, this);
+        }
+        
     }
     // Method to generate a random image for the meteor
     public Image randomImage() {
