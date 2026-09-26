c9e0396c2d602b539e97da9e05a5ed7ecc459c2c
Palmmy2351 <paramimakmaroeng@gmail.com>
2026-09-26 14:23:03 +0700

    Summary

--- MyPanel.java
+++ MyPanel.java
@@ -10,7 +10,7 @@ public class MyPanel extends JPanel{
     MyPanel() {
         setSize(690, 800);
     }
-
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(bg, 0, 0, this);
