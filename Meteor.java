44804a5c12e3eda8ba21887fc3deac096248a4e2
Yoghurt18YG <68011212001@msu.ac.th>
2026-09-26 20:39:42 +0700

    Summary

--- Meteor.java
+++ Meteor.java
@@ -76,7 +76,6 @@ public class Meteor extends Thread{
              }
         }
 
-
      public void draw(Graphics g) {
 
 
--- Mypanel.java
+++ Mypanel.java
@@ -53,4 +53,5 @@ public class Mypanel extends JPanel{
         return img;
        
     }
+    
 }
