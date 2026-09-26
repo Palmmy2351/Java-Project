8569f59114db755372a19fcdb9e78c6b580d8654
Palmmy2351 <paramimakmaroeng@gmail.com>
2026-09-26 17:09:48 +0700

    Update Meteor.java

--- Meteor.java
+++ Meteor.java
@@ -3,7 +3,6 @@ import java.awt.*;
 public class Meteor extends Thread {
     Mypanel panel;
     Image meteor;
-
     int x;
     int y;
     int speed;
@@ -17,16 +16,8 @@ public class Meteor extends Thread {
         this.x = x;
         this.y = y;
         this.speed = speed;
-<<<<<<< HEAD
-        randomDirection();
-    }
-    public void randomDirection(){
-        int[][] directions = {{1, 0},{-1, 0},{0, 1},{0, -1},{1, 1},{-1, 1},{1, -1},{-1, -1}} ;
-        int direction =(int)(Math.random()*8);
-=======
         int[][] directions = {{1, 0},{-1, 0},{0, 1},{0, -1},{1, 1},{-1, 1},{1, -1},{-1, -1}};
-        int direction = (int)(Math.random() * directions.length);
->>>>>>> 2d5635e4eb41bbd385ed6df58b4a169940f53ed4
+        int direction = (int)(Math.random() * 8);
         dx = directions[direction][0];
         dy = directions[direction][1];
     }
@@ -36,60 +27,15 @@ public class Meteor extends Thread {
         while (running) {
             x = x + dx * speed;
             y = y + dy * speed;
-<<<<<<< HEAD
-            if (x <= 0) {
-            x = 0;
-            speed++;
-            do {
-                randomDirection();
-            } while (dx < 0);
-=======
             panel.repaint();
             try {
                 Thread.sleep(30);
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
                 break;
->>>>>>> 2d5635e4eb41bbd385ed6df58b4a169940f53ed4
             }
-
-                if (x >= 640) {
-                    x = 640;
-                    speed++;
-                    do {
-                        randomDirection();
-                    } while (dx > 0);
-                }
-
-                if (y <= 0) {
-                    y = 0;
-                    speed++;
-                    do {
-                        randomDirection();
-                    } while (dy < 0);
-                }
-
-                if (y >= 750) {
-                    y = 750;
-                    speed++;
-                    do {
-                        randomDirection();
-                    } while (dy > 0);
-                }
-                    panel.repaint();
-                    try {
-                        Thread.sleep(30);
-                    } catch (InterruptedException e) {
-                        break;
-                    }
-             }
         }
-<<<<<<< HEAD
-     public void draw(Graphics g) {
-=======
     }
     public void draw(Graphics g) {
->>>>>>> 2d5635e4eb41bbd385ed6df58b4a169940f53ed4
         if (running) {
             g.drawImage(meteor,x,y,50,50,panel);
         }
