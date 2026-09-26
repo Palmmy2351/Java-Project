a729a41052390d6331c65b78355b15143a6b8a15
Palmmy2351 <paramimakmaroeng@gmail.com>
2026-09-26 14:54:56 +0700

    Summary

--- Meteor.java
+++ Meteor.java
@@ -1,4 +1,37 @@
+import java.awt.*;
 public class Meteor extends Thread{
-    
+    Mypanel panel;
 
+    Image meteor;
+
+    int x;
+    int y;
+    int speed;
+
+    boolean running = true;
+
+    Meteor(Mypanel panel, Image meteor, int x, int y, int speed) {
+        this.panel = panel;
+        this.meteor = meteor;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+    @Override
+    public void run() {
+        while (running) {
+            x = x + speed;
+            panel.repaint();
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                break;
+            }
+        }
+    }
+     public void draw(Graphics g) {
+        if (running) {
+            g.drawImage(meteor, x, y, 50, 50, panel);
+        }
+    }
 }
