79d606dc44e502e3bc275f8299318caf4eae7d3a
Yoghurt18YG <68011212001@msu.ac.th>
2026-09-26 15:10:03 +0700

    Summary

--- Mypanel.java
+++ Mypanel.java
@@ -11,6 +11,7 @@ public class MyPanel extends JPanel{
         setSize(690, 800);
     }
 
+    @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(bg, 0, 0, this);
